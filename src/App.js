ee9907934865ae3f1fd511029dcf30b069ce3268
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2023-01-06 23:19:10 +0300

    Import supplier component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Suppliers from './components/supplierProfiles/Suppliers';
 
 function App() {
   return (
     <div className="App">
       <h1>Tender Wema</h1>
+      <Suppliers />
     </div>
   );
 }
