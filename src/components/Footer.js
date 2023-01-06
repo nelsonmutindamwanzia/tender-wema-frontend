898ccb0f2cde4c54bd43bbb9a08f02c7dee200b6
wanjiku <annritawanjiku77@gmail.com>
2023-01-06 11:40:23 +0300

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import {GrDocumentPerformance} from "react-icons/gr";
+
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+        <footer>
+            <br/>
+            <div className='container-fluid' id='footer'>
+                <div className='row'>
+                    <div className='col-md-4 col-lg-4 col-sm-12 col-xl-4'>
+                        <p>  <GrDocumentPerformance/>  &nbsp; Tender Wema</p>
+                    </div>
+                    <div className='col-md-4 col-lg-4 col-sm-12 col-xl-4'>
+                         <p>Copyright 2022. TenderWema</p>
+                    </div>
+                    <div className='col-md-4 col-lg-4 col-sm-12 col-xl-4'>
+                        <ul>
+                                    <li> FB</li>
+                                    <li>lN</li>
+                                   <li>TW</li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </footer>    
+    </div>
   )
 }
 
