7ca573dadf4d333aa7a038d7ef74020743abebe9
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2023-01-06 23:55:01 +0300

    Edit supplier card

--- src/components/supplierProfiles/Suppliers.js
+++ src/components/supplierProfiles/Suppliers.js
@@ -14,15 +14,31 @@ function Suppliers() {
             <h5>Suppliers</h5>
             {suppliers.map((supplier) => {
                 return (
-                    <div class="card">
-                        <h5 class="card-header">{supplier.company_name} </h5>
-                        <div class="card-body">
-                            <p class="card-title">Company Email: {supplier.email} </p>
-                            <p class="card-text">Company Telephone: {supplier.company_telephone}</p>
-                            <p class="card-text">Company Address: {supplier.company_address}</p>
-                            {/* <a href="#" class="btn btn-primary">view</a> */}
+                    <div className="card">
+                        <h5 className="card-header">{supplier.company_name} </h5>
+                        <div className="card-body">
+                            <p className="card-title">Company Email: {supplier.email} </p>
+                            <p className="card-text">Company Telephone: {supplier.company_telephone}</p>
+                            <p className="card-text">Company Address: {supplier.company_address}</p>
+                            <a href="#" className="btn btn-primary">view</a>
                         </div>
                     </div>
+
+                    // <div className="card">
+                    //     <div className="card-body">
+                    //         <h5 className="card-title">{supplier.company_name}</h5>
+                    //     </div>
+                    //     <ul className="list-group list-group-flush">
+                    //         <li className="list-group-item">Company Email: {supplier.email}</li>
+                    //         <li className="list-group-item">Company Telephone: {supplier.company_telephone}</li>
+                    //         <li className="list-group-item">{supplier.company_address}</li>
+                    //     </ul>
+                    //     <div className="card-body">
+                    //         {/* <a href="#" class="card-link">View</a> */}
+                    //     </div>
+                    // </div>
+
+
                 )
 
             })}
