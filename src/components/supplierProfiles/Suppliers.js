72fee217b40ed7aa6c3f2286ffc481638fc7fec3
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2023-01-06 23:19:30 +0300

    fetch supplier data

--- src/components/supplierProfiles/Suppliers.js
+++ src/components/supplierProfiles/Suppliers.js
@@ -1,17 +1,31 @@
 import React, { useEffect, useState } from "react";
 
-function Suppliers () {
+function Suppliers() {
     const [suppliers, setSuppliers] = useState([]);
 
-    useEffect (() => {
-        fetch("http://127.0.0.1:3000/suppliers")
-        .then((response) => response.json())
-        .then((data) => setSuppliers(data));
+    useEffect(() => {
+        fetch("http://localhost:3000/suppliers")
+            .then((response) => response.json())
+            .then((data) => setSuppliers(data));
     }, []);
 
     return (
         <div>
-            
+            <h5>Suppliers</h5>
+            {suppliers.map((supplier) => {
+                return (
+                    <div class="card">
+                        <h5 class="card-header">{supplier.company_name} </h5>
+                        <div class="card-body">
+                            <p class="card-title">Company Email: {supplier.email} </p>
+                            <p class="card-text">Company Telephone: {supplier.company_telephone}</p>
+                            <p class="card-text">Company Address: {supplier.company_address}</p>
+                            {/* <a href="#" class="btn btn-primary">view</a> */}
+                        </div>
+                    </div>
+                )
+
+            })}
         </div>
     )
 
