5d6c9da7340a0976f31f57a41a0c636b1a17f807
nelsonmutindamwanzia <nmutinda02@gmail.com>
2023-01-08 13:09:13 +0300

    update footer code and styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,96 @@
 import React from 'react'
-import {GrDocumentPerformance} from "react-icons/gr";
-
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter, faLinkedin, faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { Link } from 'react-router-dom';
 
 function Footer() {
-  return (
-    <div>
-        <footer>
-            <br/>
-            <div className='container-fluid' id='footer'>
-                <div className='row'>
-                    <div className='col-md-4 col-lg-4 col-sm-12 col-xl-4'>
-                        <p>  <GrDocumentPerformance/>  &nbsp; Tender Wema</p>
-                    </div>
-                    <div className='col-md-4 col-lg-4 col-sm-12 col-xl-4'>
-                         <p>Copyright 2022. TenderWema</p>
-                    </div>
-                    <div className='col-md-4 col-lg-4 col-sm-12 col-xl-4'>
-                        <ul>
-                                    <li> FB</li>
-                                    <li>lN</li>
-                                   <li>TW</li>
-                        </ul>
-                    </div>
-
+    return (
+        <StyledFooter>
+            <div className="footer" id="footer">
+                <div className="ft-home">
+                    <Link to='/' className='social-logo'>
+                        Tender Wema
+                    </Link>
+                </div>
+                <div className="copy">Copyright &copy; 2023 Tender Wema</div>
+                <div className="socials">
+                    <FontAwesomeIcon className="icon" icon={faFacebook} size="xl" />
+                    <FontAwesomeIcon className="icon" icon={faLinkedin} size="xl" />
+                    <FontAwesomeIcon className="icon" icon={faTwitter} size="xl" />
                 </div>
             </div>
-        </footer>    
-    </div>
-  )
+        </StyledFooter>
+    );
 }
+
+        const StyledFooter = styled.footer`
+        margin-top: 50px;
+        height: 100px;
+        width: 100%;
+        background: #ECF7FF;
+        bottom: 0;
+        .footer {
+        display: flex;
+        width: 90%;
+        height: 100%;
+        justify-content: space-between;
+        margin-inline: auto;
+        align-items: center;
+        }
+        .copym {
+        display: none;
+        }
+        .copy {
+        text-align: center;
+        font-size: 24px;
+        font-weight: 700;
+        }
+        .ft-home{
+        font-weight: 700;
+        }
+        .socials {
+        display: flex;
+        gap: 50px;
+        }
+        .icons {
+        }
+        }
+        .logo {
+        display: flex;
+        h1,
+        span {
+        color: #000;
+        font-size: 30px;
+        font-weight: 700;
+        }
+        h1 span {
+        color: #95290b;
+        margin-right: 10px;
+        }
+        }
+        @media (max-width: 576px) {
+        display: flex;
+        flex-direction: column;
+        .footer {
+        width: 95%;
+        }
+        .socials {
+        gap: 20px;
+        color: #000;
+        }
+        .copy {
+        display: none;
+        }
+        .copym {
+        color: #000;
+        font-size: 16px;
+        font-weight: 600;
+        display: block;
+        align-self: center;
+        margin-bottom: 10px;
+        }
+        }
+        `;
 
 export default Footer
