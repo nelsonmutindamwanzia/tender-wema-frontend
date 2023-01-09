6f69b5086c79e1f9b05742b36833c2733e098e4b
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2023-01-09 22:57:16 +0300

    Add supplier component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-// import React from 'react';
-// import './App.css';
-// import Suppliers from './components/supplierProfiles/Suppliers';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Tender Wema</h1>
-//       <Suppliers />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import './App.css';
 import Navbar from './components/supplierProfiles/Navbar';
 import { Route, Routes } from 'react-router-dom';
-//import Suppliers from './components/supplierProfiles/Suppliers';
+import Suppliers from './components/supplierProfiles/Suppliers';
 
 
 function App() {
@@ -25,7 +10,7 @@ function App() {
       <>
         <Navbar />
         <Routes>
-          <Route path='/'   />
+          <Route path='/' element={<Suppliers /> } />
         </Routes>
       </>
   );
