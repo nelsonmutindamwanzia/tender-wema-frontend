88abd9a5e7d1865d7af4c75d4a661d4685c1b8e9
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2023-01-09 22:17:33 +0300

    Add sidebar to supplier page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+// import React from 'react';
+// import './App.css';
+// import Suppliers from './components/supplierProfiles/Suppliers';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Tender Wema</h1>
+//       <Suppliers />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
 import './App.css';
-import Suppliers from './components/supplierProfiles/Suppliers';
+import Navbar from './components/supplierProfiles/Navbar';
+import { Route, Routes } from 'react-router-dom';
+//import Suppliers from './components/supplierProfiles/Suppliers';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Tender Wema</h1>
-      <Suppliers />
-    </div>
+      <>
+        <Navbar />
+        <Routes>
+          <Route path='/'   />
+        </Routes>
+      </>
   );
 }
 
