3b9184b6997bc62f49fc71513c35ff16a1f853b6
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2023-01-09 22:57:03 +0300

    Edit supplier container div layout

--- src/components/supplierProfiles/Suppliers.js
+++ src/components/supplierProfiles/Suppliers.js
@@ -11,23 +11,26 @@ function Suppliers() {
     }, []);
 
     return (
-        <div className="cards-container" >
+        <div className="supplier-container" >
             <h5>Supplier Profiles</h5>
-            {suppliers.map((supplier) => {
-                return (
-                    <div className="card">
-                        <h5 className="card-header">{supplier.company_name} </h5>
-                        <div className="card-body">
-                            <p className="card-text">Company Email: <span>{supplier.email}</span> </p> 
-                            <p className="card-text">Telephone: {supplier.company_telephone}</p>
-                            <p className="card-text">Address:   {supplier.company_address}</p>
-                            <a href="#" className="btn btn-primary">view</a>
+            <div className="cards-container" >               
+                {suppliers.map((supplier) => {
+                    return (
+                        <div className="card">
+                            <h5 className="card-header">{supplier.company_name} </h5>
+                            <div className="card-body">
+                                <p className="card-text">Company Email: <span>{supplier.email}</span> </p>
+                                <p className="card-text">Telephone: {supplier.company_telephone}</p>
+                                <p className="card-text">Address:   {supplier.company_address}</p>
+                                <a href="#" className="btn btn-primary">view</a>
+                            </div>
                         </div>
-                    </div>
-                )
+                    )
 
-            })}
+                })}
+            </div>
         </div>
+
     )
 
 }
