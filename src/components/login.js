baa3617459a97c2a641f2dbe646d97210c5d2725
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2023-01-10 07:27:48 +0300

    Added email validation for the login page

--- src/components/login.js
+++ src/components/login.js
@@ -2,17 +2,26 @@ import React, { useState} from 'react'
 
 import loginImg from '../assets/Vector.png'
 // import backgrnd from '../assets/rec6.jpg'
+import validation from './Validation'
+
+
 
 export default function Login() {
-  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  
 
-  const checkEmail = () =>{
-    alert('test')
+  const [email, SetEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    if (!email.includes('@')) {
+      setError('This is an invalid email adress')
+      return;
+    }
   }
-  const [values, setValues] = useState({
-    Email: '',
-    Password: ''
-  })
+
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
        
@@ -24,17 +33,25 @@ export default function Login() {
       
 
       <div className='bg-gray-100 flex flex-col justify-center'>
-        <form className='max-w-[400px] w-full m-auto bg-white p-4'>
+        <form className='max-w-[400px] w-full m-auto bg-white p-4' onSubmit={handleSubmit}>
           <h2 className='text-4xl font-bold text-center py-6'>Hello Again!</h2>
           <div className='flex flex-col py-2'>
             <label>Email</label>
-            <input className='border p-2' type="text" value={values.Email} emailonChange={checkEmail}/>
+            <input className='border p-2' type="text" 
+            value={email}
+            onChange={(event) => SetEmail(event.target.value)}/>
           </div>
 
           <div className='flex flex-col py-2'>
             <label>Password</label>
-            <input className='border p-2' type="password"value={values.password} />
+            <input className='border p-2' type="password"
+            value={password} 
+            onChange={(event) => setPassword(event.target.value)} />
+
+            {error && <p style={{ color: 'red' }}>{error}</p>}
           </div>
+
+          
           
           <div className='flex justify-between'>
             <p className='flex items-center'><input className='mr-2' type="checkbox" /> Remember Me</p>
@@ -47,6 +64,8 @@ export default function Login() {
     </div>
   )
 }
+
+
 
 
 
