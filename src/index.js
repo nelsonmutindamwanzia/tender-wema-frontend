50f854a5457d22df149b49a51aaa62cb95fdb215
OtienohOwinoh <coowinoh@gmail.com>
2023-01-10 20:19:22 +0300

    add publish tender component

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import PublishedTender from './components/Publishtender'
 // import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    {/* <React.StrictMode> */}
+      {/* <App /> */}
+    {/* </React.StrictMode> */}
+    <Routes>
+    <Route path="/" element={<App />} />
+    <Route path="tenders/publish" element={<PublishedTender />} />
+    </Routes>
   </BrowserRouter>
 );
 
