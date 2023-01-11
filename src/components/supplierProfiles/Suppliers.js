ebf70af3d4475d5a61bbc94c8bc4d11a7f73ccee
nelsonmutindamwanzia <nmutinda02@gmail.com>
2023-01-11 15:32:26 +0300

    update proxy link to use live backend link

--- src/components/supplierProfiles/Suppliers.js
+++ src/components/supplierProfiles/Suppliers.js
@@ -8,7 +8,7 @@ function Suppliers() {
     const [searchInput, setSearchInput] = useState("");
 
     useEffect(() => {
-        fetch("http://localhost:3000/suppliers")
+        fetch("https://tender-wema-production.up.railway.app/suppliers")
             .then((response) => response.json())
             .then((data) => {
                 if (!filterQuery) {
