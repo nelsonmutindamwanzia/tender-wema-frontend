24194dc7885ee5bfa4c649f5fe31d826a2548582
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2023-01-14 22:05:18 +0300

    Update classnames

--- src/components/submittedProposals/AllProposals.js
+++ src/components/submittedProposals/AllProposals.js
@@ -44,24 +44,24 @@ function AllProposals() {
                 <br></br>
 
                 <h5>Submitted Proposals</h5>
-                <div className="cards-container" >
+                <div className="proposal-cards-container" >
                     {allProposals.map((proposal) => {
                         return (
-                            <div className='row row-cols-1 row-cols-md-3 g-4'>
-                                <div className='col'>
-                                    <div className="card">
+                            // <div className='row row-cols-1 row-cols-md-3'>
+                            //     <div className='coln'>
+                            //         <div className="cardg">
                                         <div className="proposal-card" key={proposal.id} >
-                                            <h6 className="card-title">{proposal.supplier.company_name} </h6>
-                                            <p className="card-text">Tender ID: {proposal.tender_id}</p>
-                                            <p className="card-text">Tender Name: {proposal.tender.tender_name}</p>
-                                            <p className="card-text">Proposal Amount: {proposal.proposal_amount}</p>
-                                            <p className="card-text">Email: {proposal.supplier.email} <span>{proposal.supplier.company_email}</span></p>
+                                            <h5>{proposal.supplier.company_name} </h5>
+                                            <h6>Tender ID: <strong>{proposal.tender_id}</strong></h6>
+                                            <h6>Tender Name:<strong> {proposal.tender.tender_name}</strong></h6>
+                                            <h6>Proposal Amount: <strong>{proposal.proposal_amount}</strong></h6>
+                                            <p>Email: {proposal.supplier.email}</p>
                                             
                                             <Link to={`/proposals/${proposal.id}`} ><span class="badge bg-primary">View</span></Link>
                                         </div>
-                                    </div>
-                                </div>
-                            </div>
+                            //         </div>
+                            //     </div>
+                            // </div>
 
 
                         )
