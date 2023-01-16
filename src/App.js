d2a21d889e3ec2748a5d492336db4ea87376d85c
nelsonmutindamwanzia <nmutinda02@gmail.com>
2023-01-16 14:39:28 +0300

    remove the auto-login functionality, delete unused code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import Footer from './components/Footer';
 import Landing from './components/Landing';
 import Navi from './components/Navi';
@@ -8,36 +8,20 @@ import AllProposals from './components/submittedProposals/AllProposals';
 import TenderResult from './components/Tender-Results';
 import ActiveTenders from './components/Active-Tenders';
 import PublishedTender from './components/Publishtender'
-// import Signuptest from './components/Signuptest';
-// import Logintest from './components/Logintest';
-// import 'bootstrap/dist/css/bootstrap.css'
 import Tenders from './components/Tenders/Tenders';
 import Wlogin from './components/Wlogin';
 import Wsignup from './components/Wsignup';
 import Makeproposal from './components/makeproposal/Makeproposal';
 
 function App() {
-
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    // auto-login
-    fetch("https://tender-wema-production.up.railway.app/me").then((r) => {
-      if (r.ok) {
-        r.json().then((user) => setUser(user));
-      }
-    });
-  }, []);
   return (
       <>
       <Navi/>
       
       <Routes>
         <Route path='/' element={<Landing/>}/>
-        if (!user) return (
-        <Route path='/signup' element={<Wsignup setUser={setUser} user={user}/>}/>
-        <Route path='/login' element={<Wlogin onLogin={setUser}/>}/>
-        )
+        <Route path='/signup' element={<Wsignup />}/>
+        <Route path='/login' element={<Wlogin />}/>
         <Route path='/suppliers' element={<SupplierPage/>}/>
         <Route path='/tenders/publish' element={<PublishedTender/>}/>
         <Route path='/proposals' element={<AllProposals/>}/>
