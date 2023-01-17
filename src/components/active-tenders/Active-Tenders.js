a7425a370c3a3bb7c9a01811d6dab18c1527246d
nelsonmutindamwanzia <nmutinda02@gmail.com>
2023-01-17 22:15:04 +0300

    add sidebar, GET fetch request and filter based on application_deadline

--- src/components/active-tenders/Active-Tenders.js
+++ src/components/active-tenders/Active-Tenders.js
@@ -1,40 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../Footer';
 import MakeProposalSidebar from '../makeproposal/MakeProposalSidebar';
 import './active-tenders.css';
 
-
 function ActiveTenders () {
+    const [tenders, setTenders] = useState([]);
+    const [filterQuery, setFilterQuery] = useState("");
+    const [dateFilter, setDateFilter] = useState({
+        startDate: new Date(),
+        endDate: null
+    })
+    // const filteredDates = tenders.filter(application_deadline => new Date(application_deadline) - new Date() > 0);
+    
+    useEffect(() => {
+        fetch(`https://tender-wema-production.up.railway.app/tenders`)
+            .then((response) => response.json())
+            .then((data => {
+                if (!filterQuery) {
+                    setTenders(data);
+                }
+                else {
+                    setTenders(
+                        data.filter((tender) =>
+                            tender.tender_name.toLowerCase().includes(filterQuery.toLowerCase()))
+                    )
+                }
+            }))
+    }, [filterQuery])
+
+    
+    
+
+
     return (
         <>
-        <MakeProposalSidebar/>
-        <div className='at-container'>
-            <div className='at-content'>
-                <h2> Active Tenders </h2> 
-                <div className='results-container'>
-                <div className='single-tender'>
-                    <h5> Crown Plaza Tender</h5>
-                    <table className='table table-borderless'>
-                        <tr>
-                            <td>Tender ID  </td>
-                            <td>01/2022  </td>
-                        </tr>
-                        <tr>
-                            <td> Budget  </td>
-                            <td>  $ 2000 - $ 4000 </td>
-                        </tr>
-                        <tr>
-                            <td> Timeline </td>
-                            <td> Jan 23 - Jun 23 </td>
-                        </tr>
-                        <tr>
-                            <td> Application Deadline</td>
-                            <td className='text-success'> Dec 2022</td>
-                        </tr>
-                    </table>
-                    <button> View</button>
-                </div>       
-            </div>
+        <div>
+            <MakeProposalSidebar />
+            <div className="at-container">
+                <br></br>
+                <div class="search" >
+                    <i class="fa fa-search"></i>
+                    <input
+                        type="text"
+                        placeholder="Search Tenders"
+                        onChange={(e) => {
+                            setFilterQuery(e.target.value);
+                            console.log(filterQuery);
+                        }}
+                        className="form-searchbar"
+                    />
+                </div>
+                <br></br>
+                <h5>Active Tenders</h5>
+                <div className="at-cards-container" >
+                {tenders.filter(tender => {
+                    let filterPass = true
+                    const date = new Date(tender.application_deadline)
+                    if (dateFilter.startDate) {
+                        filterPass = filterPass && (new Date(dateFilter.startDate) < date)
+                    }
+                    if (dateFilter.endDate) {
+                        filterPass = filterPass && (new Date(dateFilter.endDate) > date)
+                    }
+                    //if filterPass comes back `false` the row is filtered out
+                    return filterPass
+                    }).map((tender) => {
+                        return (
+                            <div>
+                                <div className="at-card" key={tender.id} >
+                                    <h5>{tender.tender_name} </h5>
+                                    <h6>Tender Category: <strong>{tender.tender_category}</strong></h6>
+                                    <h6>Tender Budget: <strong>{tender.budget}</strong></h6>
+                                    <h6>Timeline: <strong>{tender.timeline}</strong></h6>
+                                    <h6>Application Deadline: <strong> {tender.application_deadline}</strong></h6>
+                                    {/* <p>Description: {tender.tender_description}</p> */}
+                                    {/* <Link to={`/tenders/${tender.id}`} ><span class="badge bg-primary">View Tender</span></Link> */}
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         </div>
         <Footer/>
