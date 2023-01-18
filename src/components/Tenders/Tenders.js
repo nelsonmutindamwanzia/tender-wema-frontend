81cb4172db09735aefcffb9d12fd097027dd1b92
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2023-01-18 18:20:43 +0300

    change APIHost to local

--- src/components/Tenders/Tenders.js
+++ src/components/Tenders/Tenders.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import SupplierSideBar from "../supplierProfiles/SupplierSideBar";
 import '../supplierProfiles/supplier.css';
-import {FaSearch} from "react-icons/fa"
+import { FaSearch } from "react-icons/fa"
 import Footer from "../Footer";
 
 
@@ -11,7 +11,7 @@ function Tenders() {
     const [filterQuery, setFilterQuery] = useState("");
 
     useEffect(() => {
-        fetch(`https://tender-wema-production.up.railway.app/tenders`)
+        fetch(`http://127.0.0.1:3000/tenders`)
             .then((response) => response.json())
             .then((data => {
                 if (!filterQuery) {
@@ -30,23 +30,23 @@ function Tenders() {
         <div>
             <SupplierSideBar />
             <div className="tenders-container">
-                <br/><br/>
+                <br /><br />
                 <div className="search" >
                     <FaSearch style={{
-                                    color: "#5A96F9",
-                                    }}/>                    
+                        color: "#5A96F9",
+                    }} />
                     <input
                         type="text"
                         placeholder="Search Supplier"
                         onChange={(e) => {
                             setFilterQuery(e.target.value);
-                            console.log(filterQuery);
+                            console.log(setFilterQuery);
                         }}
                         className="form-searchbar"
                     />
                 </div>
                 <br></br>
-                
+
                 <h5>All Tenders</h5>
                 <div className="tenders-cards-container" >
                     {tenders.map((tender) => {
@@ -57,39 +57,37 @@ function Tenders() {
                                 <div className="tenders-card" key={tender.id} >
                                     <h5>{tender.tender_name} </h5>
                                     <table className="table">
-                                                                <tbody>
-                                                                    <tr>
-                                                                    <td>Tender Category</td>
-                                                                    <td>{tender.tender_category}</td>
-                                                                    </tr>
-                                                                    <tr>
-                                                                    <td>Tender Budget:</td>
-                                                                    <td>{tender.budget}</td>
-                                                                    </tr>
-                                                                    <tr>
-                                                                    <td>Timeline:</td>
-                                                                    <td>{timeline}</td>
-                                                                    </tr>
-                                                                    <tr>
-                                                                    <td>Application Deadline:</td>
-                                                                    <td> {deadline}</td>
-                                                                    </tr>
-                                                                </tbody>
+                                        <tbody>
+                                            <tr>
+                                                <td>Tender Category</td>
+                                                <td>{tender.tender_category}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Tender Budget:</td>
+                                                <td>{tender.budget}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Timeline:</td>
+                                                <td>{timeline}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Application Deadline:</td>
+                                                <td> {deadline}</td>
+                                            </tr>
+                                        </tbody>
                                     </table>
                                     <div className="btnholder">
-                                        <Link to={`/tenders/${tender.id}`} ><span class="btn" id="submittender">Submit Proposal</span></Link>
+                                        <Link to={`/tenders/${tender.id}`} ><span class="btn" id="submittender">View Proposals</span></Link>
                                     </div>
                                 </div>
                             </div>
-
-
                         )
 
                     })}
                 </div>
 
             </div>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
